f6c3485d8dc526d4de8d0f708d658ceed92e9df1
Jayakumar Babu <jaybabu@Jayakumars-MBP.attlocal.net>
2024-08-01 16:30:50 -0500

    Adding modal for create new repo

--- src/app/shared/ui/pagetitle/pagetitle.component.ts
+++ src/app/shared/ui/pagetitle/pagetitle.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'app-page-title',
@@ -6,13 +7,24 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./pagetitle.component.scss']
 })
 export class PagetitleComponent implements OnInit {
+  modalRef?: BsModalRef;
 
   @Input() breadcrumbItems;
   @Input() title: string;
 
-  constructor() { }
+  constructor(private modalService: BsModalService) { }
 
   ngOnInit() {
   }
 
+  /**
+   * Open modal
+   * @param content modal content
+   */
+  openModal(content: any) {
+    this.modalRef = this.modalService.show(content);
+  }
+  disableLink(event: Event) {
+    event.preventDefault(); 
+  }
 }
