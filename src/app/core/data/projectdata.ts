79a5cf615a7f4e1feb33f9da3db3684eeec8f5f7
Jayakumar Babu <jaybabu@Jayakumars-MBP.attlocal.net>
2024-08-01 00:11:12 -0500

    Initial Commit

---
+++ src/app/core/data/projectdata.ts
@@ -0,0 +1,136 @@
+const projectData = [
+    {
+        id: 1,
+        image: 'assets/images/companies/img-1.png',
+        appname: 'BE',
+        text: 'BillEase-Core',
+        subtext: 'Bill Ease applications Core module',
+        users: ['assets/images/usr/avatar3.jpeg', 'assets/images/usr/avatar2.jpeg'],
+        status: 'SAST Scan Completed',
+        date: '31 Jul, 24',
+        comment: 15
+    },
+    {
+        id: 2,
+        image: 'assets/images/companies/img-2.png',
+        appname: 'CC',
+        text: 'Customer-Care-UI',
+        subtext: 'UI code repo for CCA',
+        users: ['assets/images/usr/avatar3.jpeg'],
+        status: 'SCA Scan Completed',
+        date: '30 Jul, 24',
+        comment: 5
+    },
+    {
+        id: 3,
+        image: 'assets/images/companies/img-3.png',
+        appname: 'AD',
+        text: 'Adserver-App',
+        subtext: 'Code repository for Adserver',
+        users: ['assets/images/usr/avatar9.jpeg', 'assets/images/usr/avatar4.jpeg'],
+        status: 'DAST Scan Completed',
+        date: '31 Jul, 24',
+        comment: 11
+    },
+    {
+        id: 4,
+        image: 'assets/images/companies/img-4.png',
+        appname: 'NIC',
+        text: 'Network-Inventory-Common',
+        subtext: 'Network Inventory code repo',
+        users: ['assets/images/usr/avatar6.jpeg', 'assets/images/usr/avatar9.jpeg', 'assets/images/usr/avatar3.jpeg'],
+        status: 'SAST Scan Completed',
+        date: '29 Jul, 24',
+        comment: 22
+    },
+    {
+        id: 5,
+        image: 'assets/images/companies/img-5.png',
+        appname: 'WFM',
+        text: 'Workflow-Manager',
+        subtext: 'WFM code repository',
+        users: ['assets/images/usr/avatar7.jpeg', 'assets/images/usr/avatar8.jpeg'],
+        status: 'DAST Scan Completed',
+        date: '22 Jul, 24',
+        comment: 12
+    },
+    {
+        id: 6,
+        image: 'assets/images/companies/img-6.png',
+        appname: 'AM',
+        text: 'Asset-Manager-Core',
+        subtext: 'Asset Manager Code Repo',
+        users: ['assets/images/usr/avatar6.jpeg'],
+        status: 'SCA Scan Completed',
+        date: '24 Jul, 24',
+        comment: 6
+    },
+    {
+        id: 7,
+        image: 'assets/images/companies/img-3.png',
+        appname: 'BCM',
+        text: 'BCM-APP-Repo',
+        subtext: 'BCM application code repo',
+        users: ['assets/images/usr/avatar3.jpeg'],
+        status: 'DAST Scan Completed',
+        date: '31 Jul, 24',
+        comment: 18
+    },
+    {
+        id: 8,
+        image: 'assets/images/companies/img-4.png',
+        appname: 'IC',
+        text: 'IoT-Connect',
+        subtext: 'Code repository for IOTC',
+        users: ['assets/images/usr/avatar4.jpeg'],
+        status: 'SCA Scan Completed',
+        date: '27 Jul, 24',
+        comment: 5
+    },
+    {
+        id: 9,
+        image: 'assets/images/companies/img-2.png',
+        appname: 'AS',
+        text: 'AssitTel-Service',
+        subtext: 'Assist Tel Service code repo',
+        users: ['assets/images/usr/avatar7.jpeg'],
+        status: 'SAST Scan Completed',
+        date: '29 Jul, 24',
+        comment: 16
+    },
+    {
+        id: 10,
+        image: 'assets/images/companies/img-6.png',
+        appname: 'NC',
+        text: 'NetCloud-Common',
+        subtext: 'Net Cloud application code repo',
+        users: ['assets/images/usr/avatar6.jpeg'],
+        status: 'DAST Scan Completed',
+        date: '24 Jul, 24',
+        comment: 12
+    },
+    {
+        id: 11,
+        image: 'assets/images/companies/img-3.png',
+        appname: 'SS',
+        text: 'Signal-Sync',
+        subtext: 'Signal Sync application repo',
+        users: ['assets/images/usr/avatar3.jpeg'],
+        status: 'SAST Scan Completed',
+        date: '30 Jul, 24',
+        comment: 21
+    },
+    {
+        id: 12,
+        image: 'assets/images/companies/img-4.png',
+        appname: 'UC',
+        text: 'UConnect-App',
+        subtext: 'Unified Connect App repo',
+        users: ['assets/images/usr/avatar6.jpeg', 'assets/images/usr/avatar4.jpeg', 'assets/images/usr/avatar3.jpeg'],
+        status: 'DAST Scan Completed',
+        date: '29 Jul, 24',
+        comment: 13
+    },
+];
+
+export { projectData };
