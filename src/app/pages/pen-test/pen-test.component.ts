2d98ebbc30e2fa1051f8aebb8fa316ecdfb7397f
Jayakumar Babu <jaybabu@Jayakumars-MBP.attlocal.net>
2024-08-02 15:38:34 -0500

    PenTest Dashboard

--- src/app/pages/pen-test/pen-test.component.ts
+++ src/app/pages/pen-test/pen-test.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { penTestData } from 'src/app/core/data/penTestData';
 
 @Component({
   selector: 'app-pen-test',
   templateUrl: './pen-test.component.html',
   styleUrls: ['./pen-test.component.css']
 })
-export class PenTestComponent {
-
+export class PenTestComponent implements OnInit {
+  dataToUse = penTestData;
+  ngOnInit() {
+    
+  }
 }
